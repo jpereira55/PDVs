49218bb632fe6a435bb2ed17aae59cd56ce2695e
jpereira55 <129891766+jpereira55@users.noreply.github.com>
2023-04-05 12:45:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ pdvsJson.map((pdv) => {
     pdvsclone.querySelector('.card p').innerHTML = pdv.descripion;
 
 
-    if(pdv.descripion === 'Drive') {
+    if(pdv.descripion === 'DRIVE') {
       pdvsclone.querySelector('.card').style.background = '#E19200';
     }
     if(pdv.name === 'PDV 26') {
